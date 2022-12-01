02f563ef2301ff8a31249696c799e5ecce3005f5
Anastasiia <113987750+Anastasiia2205@users.noreply.github.com>
2022-12-01 21:07:08 +0300

    Update 1.py

--- 7 практика/1.py
+++ 7 практика/1.py
@@ -1,12 +1,18 @@
-pos_min, pos_max, value_min, value_max = 0, 0, l[0], l[0]
+# -- coding: utf-8 --
+#def f():
+#   a = [1, 2, 3 ,123]
+#   print(a)
+#   print(sum(a))
+#   result = 1
+#   for i in a:
+#      result *= i
+#   print(result)
+#f()
 
-for i in range(len(l)):
- if l[i] < value_min:
- pos_min = i
- value_min = l[i]
 
- if l[i] > value_max:
- pos_max = i
- value_max = l[i]
-
-l[pos_min], l[pos_max] = value_max, value_min
+def f():
+   num = [3, 3, 0, 0, 0, 3, 3]
+   avg= sum(num) / len(num)
+   num = [avg if i == 0 else i for i in num]
+   print(num)
+f()
